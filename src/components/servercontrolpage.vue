e9af3de9ad4a7930686c031479b4508dcb2fab9c
KieranWaigel <30296791+KieranWaigel@users.noreply.github.com>
2020-12-19 23:11:34 -0500

    nowdidthis work
    pls help

---
+++ src/components/servercontrolpage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    control page goes here
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'servercontrol',
+  data() {
+      return{
+         msg: String
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
